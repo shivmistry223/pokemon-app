3491d06243068b165dc27e3cc0e92e7d5602e524
Shiv Mistry <shivmistry223@gmail.com>
2025-08-14 12:36:20 -0400

    Integrated pokemon detail

--- components/Pokemon/PokemonCard.tsx
+++ components/Pokemon/PokemonCard.tsx
@@ -1,35 +1,76 @@
-import { StyleSheet, View, Text, Platform, Image } from "react-native";
+import { StyleSheet, View, Text, Platform, Image, Pressable, Modal } from "react-native";
 import { getTypeDetails } from "./Utils";
+import { useState } from "react";
 
 export default function PokemonCard({ name = "", image = require("../../assets/images/pikachu.png"), type = "", hp = 0, moves = [""], weakness = [""] }) {
     const { borderColor, emoji } = getTypeDetails(type)
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
-        <View style={styles.card}>
-            <View style={styles.firstContainer}>
-                <Text style={styles.name}>{name}</Text>
-                <Text style={styles.hp}>❤️{hp}</Text>
-            </View>
-            <Image source={image} style={styles.image} accessibilityLabel={`${name} pokemon`} />
-            <View style={styles.secondContainer}>
-                <View style={[styles.innerSecondContainer, { borderColor: borderColor }]}>
-                    <Text>{emoji}</Text>
-                    <Text style={{ color: borderColor }}>{type}</Text>
+        <Pressable
+            onPress={() => setModalVisible(true)}>
+            <View style={styles.card}>
+                <View style={styles.firstContainer}>
+                    <Image source={image} style={styles.image} accessibilityLabel={`${name} pokemon`} />
+                    <Text style={styles.name}>{name}</Text>
                 </View>
             </View>
-            <View>
-                <Text style={styles.boldText}>Moves: {moves.join(",")}</Text>
-            </View>
-            <View>
-                <Text style={styles.boldText}>Weakness: {weakness.join(",")}</Text>
-            </View>
-        </View>)
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <View style={styles.modalFirstContainer}>
+                            <Text style={styles.name}>{name}</Text>
+                            <Text style={styles.hp}>❤️{hp}</Text>
+                        </View>
+                        <View style={styles.imageContainer}>
+                            <Image source={image} style={styles.modalImage} accessibilityLabel={`${name} pokemon`} />
+                        </View>
+                        <View style={styles.secondContainer}>
+                            <View style={[styles.innerSecondContainer, { borderColor: borderColor }]}>
+                                <Text>{emoji}</Text>
+                                <Text style={{ color: borderColor }}>{type}</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text>When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.boldText}>Moves</Text>
+                            <View style={styles.moveContainer}>
+                                {moves.map((move, index) => <Text style={[styles.move, { backgroundColor: borderColor }]} key={index}>{move}</Text>)}
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.boldText}>Weakness</Text>
+                            <View style={styles.moveContainer}>
+                                {weakness.map((weak, index) => <Text style={[styles.move, { backgroundColor: "red" }]} key={index}>{weak}</Text>)}
+                            </View>
+                        </View>
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>Close</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+        </Pressable>
+    )
+
 }
 
 const styles = StyleSheet.create({
     card: {
         backgroundColor: "#f5f5f5",
-        padding: 16,
-        margin: 16,
+        padding: 9,
+        margin: 9,
         gap: 10,
         borderRadius: 16,
         ...Platform.select({
@@ -39,15 +80,16 @@ const styles = StyleSheet.create({
             ios: {
                 //ios code
             }
-        })
+        }),
+        width: 150,
+        height: 150
     },
     firstContainer: {
-        flexDirection: "row",
-        alignContent: "center",
-        justifyContent: "space-between",
+        justifyContent: "center",
+        alignItems: "center"
     },
     name: {
-        fontSize: 28,
+        fontSize: 18,
         fontWeight: "bold"
     },
     hp: {
@@ -55,7 +97,16 @@ const styles = StyleSheet.create({
     },
     image: {
         width: "100%",
-        height: 200,
+        height: 90,
+        resizeMode: "contain"
+    },
+    imageContainer: {
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    modalImage: {
+        height: 150,
+        width: 150,
         resizeMode: "contain"
     },
     secondContainer: {
@@ -83,4 +134,60 @@ const styles = StyleSheet.create({
         fontSize: 18,
 
     },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        gap: 10
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    modalFirstContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    moveContainer: {
+        gap: 5,
+        flexDirection: "row",
+        flexWrap: "wrap",
+
+    },
+    move: {
+        borderRadius: 12,
+        paddingHorizontal: 10,
+        paddingVertical: 4,
+        fontWeight: "600"
+    }
 })
--- components/Pokemon/PokemonMain.tsx
+++ components/Pokemon/PokemonMain.tsx
@@ -1,75 +1,43 @@
 import { Image, ScrollView, StyleSheet, View } from "react-native";
 import PokemonCard from "./PokemonCard";
-
+import { pokemonLogo, pokemonDataArray } from "./constant"
 
 export default function PokemonMain() {
-    const pikachuImg = require("../../assets/images/pikachu.png")
-    const pokemonLogo = require("../../assets/images/pokemon_logo.png")
-    const pokemonDataArray = [
-        {
-            name: "Pikachu",
-            image: pikachuImg,
-            type: "Electric",
-            hp: 35,
-            moves: ["Thunder Shock", "Quick Attack"],
-            weakness: ["Ground"]
-        },
-        {
-            name: "Charmander",
-            image: require("../../assets/images/charmander.png"),
-            type: "Fire",
-            hp: 39,
-            moves: ["Ember", "Scratch"],
-            weakness: ["Water", "Rock", "Ground"]
-        },
-        {
-            name: "Bulbasaur",
-            image: require("../../assets/images/bulbasaur.png"),
-            type: "Grass",
-            hp: 45,
-            moves: ["Vine Whip", "Tackle"],
-            weakness: ["Fire", "Ice", "Flying", "Psychic"]
-        },
-        {
-            name: "Squirtle",
-            image: require("../../assets/images/squirtle.png"),
-            type: "Water",
-            hp: 44,
-            moves: ["Water Gun", "Tackle"],
-            weakness: ["Electric", "Grass"]
-        },
-        {
-            name: "Eevee",
-            image: require("../../assets/images/eevee.png"),
-            type: "Normal",
-            hp: 55,
-            moves: ["Quick Attack", "Bite"],
-            weakness: ["Fighting"]
-        }
-    ];
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.firstContainer}>
-                    <Image style={styles.image} source={pokemonLogo} />
-                </View>
-                {pokemonDataArray.map((pokemon, index) => <PokemonCard key={index} {...pokemon} />)}
+        <View style={styles.container}>
+            <View style={styles.firstContainer}>
+                <Image style={styles.image} source={pokemonLogo} />
             </View>
-        </ScrollView>)
+            <ScrollView>
+                <View style={styles.secondContainer}>
+                    {pokemonDataArray.map((pokemon, index) => <PokemonCard key={index} {...pokemon} />)}
+                    {pokemonDataArray.map((pokemon, index) => <PokemonCard key={index} {...pokemon} />)}
+                </View>
+            </ScrollView>
+        </View>)
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#f5f5f5",
-        gap: 10
+        gap: 10,
+        flexWrap: "wrap"
     },
     firstContainer: {
         flexDirection: "row",
         justifyContent: "center",
         alignContent: "center",
         margin: 10
+    },
+    secondContainer: {
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 10,
     },
     image: {
         width: "100%",
